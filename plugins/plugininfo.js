6af69cf5dc9312b7f2a81f4b25e255eb2c74b2e6
slackerSnail <ajc3860@gmail.com>
2016-05-15 01:29:20 -0400

    Create plugininfo.js

---
+++ plugins/plugininfo.js
@@ -0,0 +1,37 @@
+/*
+ * Plugin Info Checker
+ * by slackerSnail
+ */
+function _pluginInfo(name) {
+  var xhr = new XMLHttpRequest;
+  var json, message;
+  xhr.open("http://ShadowC-X-11.github.io/SBS-chatJS-Plugins/info/" + name + ".json?t=" + (new Date()).getTime(), false);
+  xhr.send();
+  
+  if(xhr.status == 404) {
+    warningMessage("Plugin info not found!");
+  } else {
+    try {
+      json = JSON.parse(xhr.responseText);
+    } catch (e) {
+      warningMessage("Plugin info error: " + e.message);
+    }
+    //if we're all clear, begin parsing the info.
+    message += json.name + "\n" + json.description + "\n*****\n\nCommands\n=====\n";
+    json.commands.forEach(function(cmd) {
+      message += cmd.name + "\n" + cmd.description + "\n-----\n";
+    });
+    moduleMessage(message);
+  }
+}
+
+function init() {
+  addCommand("plugininfo", function(arg) {
+    var params = quickParamParse(arg);
+    if(params.length != 1) {
+     warningMessage("Incorrect args!");
+    } else {
+     _pluginInfo(params[0]);
+    }
+  }, "Gets information on the named plugin.");
+}
