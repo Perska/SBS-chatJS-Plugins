af87b1f037a9ed10ac10915c684c156a34fa8021
ShadowCX11 <shadowcx11sb@gmail.com>
2016-05-14 23:40:56 -0300

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -2,12 +2,12 @@
 // Put this in your chatJS
 // By ShadowCX11 & slackerSnail, 2016
 var xhr = new XMLHttpRequest;
-xhr.open("GET", "http://ShadowC-X-11.github.io/SBS-chatJS-Plugins/required.json", false);
+xhr.open("GET", "http://ShadowC-X-11.github.io/SBS-chatJS-Plugins/required.json?t=" + (new Date()).getTime(), false);
 xhr.send();
 var modules = JSON.parse(xhr.responseText);
 modules.forEach(function(name){
 	var x = new XMLHttpRequest;
-	x.open("GET", "http://ShadowC-X-11.github.io/SBS-chatJS-Plugins/" + name, false);
+	x.open("GET", "http://ShadowC-X-11.github.io/SBS-chatJS-Plugins/" + name + "?t=" + (new Date()).getTime(), false);
 	x.send();
 	eval(x.responseText);
 });
